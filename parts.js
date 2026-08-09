0517c0adde15b2941630848e6759a331567752b6
Sait Firat NEMIS <sfnemis@gmail.com>
2026-08-09 04:15:47 +0300

    feat: guides hub with four articles, Turkish and German landing pages with hreflang, sitemap and nav updates

--- parts.js
+++ parts.js
@@ -16,6 +16,7 @@
     '<a href="https://www.youtube.com/@macdioapp" target="_blank" rel="noopener" aria-label="Macdio on YouTube"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 00-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 00.502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 002.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 002.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg></a>';
 
   var MORE_LINKS =
+    '<a href="' + P + 'guides/">Guides</a>' +
     '<a href="' + P + 'iphone.html">Radio for iPhone</a>' +
     '<a href="' + P + 'mac.html">Radio for Mac</a>' +
     '<a href="' + P + 'apple-tv.html">Radio for Apple TV</a>' +
@@ -50,6 +51,7 @@
       '<a class="mm-link" href="' + HOME + '#identify">Features</a>' +
       '<a class="mm-link" href="' + HOME + '#preview">Screenshots</a>' +
       '<a class="mm-link nav-livedio" href="https://livedio.app" target="_blank" rel="noopener"><img src="' + A + 'assets/livedio-mark.svg" alt="Livedio"><span>Livedio</span></a>' +
+      '<a class="mm-small" href="' + P + 'guides/">Guides</a>' +
       '<a class="mm-small" href="' + P + 'iphone.html">Radio for iPhone</a>' +
       '<a class="mm-small" href="' + P + 'mac.html">Radio for Mac</a>' +
       '<a class="mm-small" href="' + P + 'apple-tv.html">Radio for Apple TV</a>' +
